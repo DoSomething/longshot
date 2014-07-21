d3fe4a0e0ae39da68a1529853f92a4eafcea18e7
Diego Lorenzo <dlorenzo@dosomething.org>
2014-07-21 17:01:49 -0400

    Adding initial routes and starting some basic Views setup.

--- app/routes.php
+++ app/routes.php
@@ -15,3 +15,42 @@ Route::get('/', function()
 {
 	return View::make('hello');
 });
+
+
+/**
+ * Setting up some initial routes.
+ */
+Route::get('about', function()
+{
+	return 'This is the About page';
+});
+
+
+Route::get('faq', function()
+{
+	return 'This is the FAQs page';
+});
+
+
+Route::get('application', function()
+{
+	return 'This is the Application page';
+});
+
+
+Route::get('recommendation', function()
+{
+	return 'This is the Recommendations page';
+});
+
+
+Route::get('status', function()
+{
+	return 'This is the Status page';
+});
+
+
+Route::get('admin', function()
+{
+	return 'This is the Admin page';
+});
---
+++ app/views/master.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<title>Scholarship Application</title>
+	</head>
+
+	<body>
+		
+		@yield(content)
+
+	</body>
+</html>
