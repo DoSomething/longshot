b830ae432994462ddd0f2de747ab0fad148d2204
Diego Lorenzo <dlorenzo@dosomething.org>
2014-08-06 10:41:58 -0400

    Removed the Commander package for now since it's use requires a much more advanced knowledge of the Command Pattern and DDD. Sooooooon.

--- app/config/app.php
+++ app/config/app.php
@@ -122,7 +122,6 @@ return array(
     'Illuminate\View\ViewServiceProvider',
     'Illuminate\Workbench\WorkbenchServiceProvider',
     'Way\Generators\GeneratorsServiceProvider',
-    'Laracasts\Commander\CommanderServiceProvider',
 
   ),
 
