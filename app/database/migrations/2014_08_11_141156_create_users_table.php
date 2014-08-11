afbefc2eae28fcc7b47af33a2c4342e1c93cc495
Diego Lorenzo <dlorenzo@dosomething.org>
2014-08-11 11:35:19 -0400

    Adding intial Users schema migration and updating ReadMe.
    Tabs issue again.

---
+++ app/database/migrations/2014_08_11_141156_create_users_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration {
+
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('users', function(Blueprint $table)
+    {
+
+      $table->increments('uid');
+      $table->string('email')->unique;
+      $table->string('password');
+      $table->string('role');
+      $table->string('first_name');
+      $table->string('last_name');
+      $table->date('birthdate');
+      $table->string('phone');
+      $table->string('address_street');
+      $table->string('address_premise')->nullable();
+      $table->string('city');
+      $table->string('state');
+      $table->integer('zip');
+      $table->string('gender')->nullable();
+      $table->string('race')->nullable();
+      $table->string('school')->nullable();
+      $table->integer('grade')->nullable();
+      $table->timestamps();
+
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+
+    Schema::drop('users');
+
+  }
+
+}
