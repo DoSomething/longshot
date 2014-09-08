3562b85912df271a97c180a3044c9788f3dcdbfc
Andrea Gaither <angaither@gmail.com>
2014-09-08 12:54:34 -0400

    Adding/removing form elements dynamically.

--- app/views/admin/page/partials/_form_page.blade.php
+++ app/views/admin/page/partials/_form_page.blade.php
@@ -24,32 +24,32 @@
 
   {{-- Block item grouping --}}
   <div class="well">
-  <div class="repeatable">
+    <div class="repeatable">
 
-  {{-- Block title --}}
-    <div class="field-group">
-      {{ Form::label('blocks[0]["title"]', 'Block Title: ') }}
-      {{ Form::text('blocks[0]["title"]') }}
-      {{ errorsFor('blocks[0]["title"]', $errors); }}
+    {{-- Block title --}}
+      <div class="field-group">
+        {{ Form::label('blocks[0]["title"]', 'Block Title: ') }}
+        {{ Form::text('blocks[0]["title"]') }}
+        {{ errorsFor('blocks[0]["title"]', $errors); }}
+      </div>
+
+      {{-- Block desc --}}
+      <div class="field-group">
+        {{ Form::label('blocks[0]["description"]', 'Block Description: ') }}
+        {{ Form::textarea('blocks[0]["description"]') }}
+        {{ errorsFor('blocks[0]["description"]', $errors); }}
+      </div>
+
+      {{-- Block body --}}
+      <div class="field-group">
+        {{ Form::label('blocks[0]["body"]', 'Block Body: ') }}
+        {{ Form::textarea('blocks[0]["body"]') }}
+        {{ errorsFor('blocks[0]["body"]', $errors); }}
+      </div>
+      <button href="#" class ="btn remove"> Remove this block</button>
     </div>
+    <div class="wrapper"></div>
 
-    {{-- Block desc --}}
-    <div class="field-group">
-      {{ Form::label('blocks[0]["description"]', 'Block Description: ') }}
-      {{ Form::textarea('blocks[0]["description"]') }}
-      {{ errorsFor('blocks[0]["description"]', $errors); }}
-    </div>
-
-    {{-- Block body --}}
-    <div class="field-group">
-      {{ Form::label('blocks[0]["body"]', 'Block Body: ') }}
-      {{ Form::textarea('blocks[0]["body"]') }}
-      {{ errorsFor('blocks[0]["body"]', $errors); }}
-    </div>
-
-    <button href="#" class ="btn remove"> Remove this block</button>
-  </div>
-  {{-- @TODO: this needs to come out of the well, but I got the cloning working so here we are. --}}
   <button href="#" class ="btn clone"> Add another block</button>
   </div>
 
@@ -57,23 +57,28 @@
 
 {{--@TODO: take this outta here. --}}
 <script type="text/javascript">
-  var regex = /^(.*)(\d)+$/i;
   var cloneIndex = $(".repeatable").length - 1;
+  var repeatableForm = $(".repeatable");
   $("button.clone").on("click", function(e){
       e.preventDefault();
-      newBlock = $(this).prev().clone()
-                     .appendTo(".well")
-                     .attr("class", "clone repeatable")
       cloneIndex++;
+      newClass = "cloned" + cloneIndex;
+      newBlock = repeatableForm.clone()
+                     .appendTo(".wrapper")
+                     .attr("class", newClass);
+
       // Replace all instances of [0] with cloneIndex
-      $.each($(".clone .field-group :input"), function() {
-        newId = $(this).attr("id").replace(cloneIndex-1, cloneIndex);
-        $(this).attr( "id", newId)
-               .attr("name", newId);
+      $.each($(".wrapper ." + newClass + " .field-group"), function() {
+        inputFields = $(this).find(":input");
+        newId = inputFields.attr("id").replace(0, cloneIndex);
+        inputFields.attr('id', newId).attr('name', newId);
+        $(this).find("label").attr("for", newId);
+
       });
   });
 
-  $("button.remove").on("click", function(e){
-    $(this).parent(".repeatable").remove();
+  $(".well").on("click", "button.remove", function(e){
+    e.preventDefault();
+    $(this).parent().remove();
   });
 </script>
