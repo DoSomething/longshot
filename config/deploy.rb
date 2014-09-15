1d5b3f9ee437564fe6930e7becb89bd4a186905a
Morgan Rich <mrich@dosomething.org>
2014-09-15 12:41:33 -0400

    Adding symlink step to cap deploy to link settings file from shared

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,3 +14,15 @@ set :scm, :none
 set :deploy_via, :copy
 
 ssh_options[:keys] = [ENV["CAP_PRIVATE_KEY"]]
+
+namespace :deploy do
+
+  task :link_settings do
+    on roles(:app) do |host|
+      execute "ln -nfs #{shared_path}/shared/.env.php #{release_path}/"
+    end
+  end
+
+  after :symlink, 'deploy:link_settings'
+
+end
