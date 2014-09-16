686d22ffefc4e053c92e7017f7c3a72830161380
Andrea Gaither <angaither@gmail.com>
2014-09-16 11:32:21 -0400

    Updated the eligibility to use tos validation.

--- app/Scholarship/Forms/RegistrationForm.php
+++ app/Scholarship/Forms/RegistrationForm.php
@@ -14,6 +14,7 @@ class RegistrationForm extends FormValidator {
     'last_name'  => 'required',
     'email'      => 'required|email|unique:users',
     'password'   => 'required|confirmed',
+    'eligibility'=> 'required|accepted',
   ];
 
 }
