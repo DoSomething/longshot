06c6ddb1e6da753c487f427f434a8b4e5c46b365
Andrea Gaither <angaither@gmail.com>
2014-09-19 17:17:55 -0400

    Commented out broken email request, waiting on new copy?

--- app/controllers/StatusController.php
+++ app/controllers/StatusController.php
@@ -25,24 +25,25 @@ class StatusController extends \BaseController {
   //@TODO refactor, move and combine into one function... etc etc. this code is the worst.
   public function resendEmailRequest()
   {
-    $rec_id = Input::get('id');
-    $recommendation = Recommendation::whereId($rec_id)->firstOrFail();
-    $to = $recommendation->email;
-    $from = Auth::user()->first_name .  " " . Auth::user()->last_name;
-    $scholarship = Scholarship::firstOrFail()->pluck('title'); // @TODO: get the real scholarship
-    $subject = $from . " would like you pleaseeee to to be a recommender for the " . $scholarship . " scholarship";
+    // @todo: redo this depending on copy.
+    // $rec_id = Input::get('id');
+    // $recommendation = Recommendation::whereId($rec_id)->firstOrFail();
+    // $to = $recommendation->email;
+    // $from = Auth::user()->first_name .  " " . Auth::user()->last_name;
+    // $scholarship = Scholarship::firstOrFail()->pluck('title'); // @TODO: get the real scholarship
+    // $subject = $from . " would like you pleaseeee to to be a recommender for the " . $scholarship . " scholarship";
 
-    $data = array(
-      'to' => $to,
-      'subject' => $subject,
-      'applicant' => $from,
-      'recommendation_id' => $recommendation->id,
-      'token' => $recommendation->generateRecToken($recommendation)
-    );
-    Mail::send('emails.recommendation.request', $data, function($message) use ($data)
-    {
-      $message->to($data['to'])->subject($data['subject']);
-    });
+    // $data = array(
+    //   'to' => $to,
+    //   'subject' => $subject,
+    //   'applicant' => $from,
+    //   'recommendation_id' => $recommendation->id,
+    //   'token' => $recommendation->generateRecToken($recommendation)
+    // );
+    // Mail::send('emails.recommendation.request', $data, function($message) use ($data)
+    // {
+    //   $message->to($data['to'])->subject($data['subject']);
+    // });
 
     return Redirect::route('status')->with('flash_message', 'We sent another email!');
   }
