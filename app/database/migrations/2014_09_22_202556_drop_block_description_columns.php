c1b357cd38257823d242de6da821e3665531bf77
Diego Lorenzo <dlorenzo@dosomething.org>
2014-09-22 16:52:52 -0400

    Update to spacingz.

--- app/database/migrations/2014_09_22_202556_drop_block_description_columns.php
+++ app/database/migrations/2014_09_22_202556_drop_block_description_columns.php
@@ -5,31 +5,31 @@ use Illuminate\Database\Migrations\Migration;
 
 class DropBlockDescriptionColumns extends Migration {
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::table('blocks', function(Blueprint $table)
-		{
-			$table->dropColumn(['block_description', 'block_description_html']);
-		});
-	}
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::table('blocks', function(Blueprint $table)
+    {
+      $table->dropColumn(['block_description', 'block_description_html']);
+    });
+  }
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::table('blocks', function(Blueprint $table)
-		{
-			$table->longtext('block_description');
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::table('blocks', function(Blueprint $table)
+    {
+      $table->longtext('block_description');
       $table->longtext('block_description_html');
-		});
-	}
+    });
+  }
 
 }
--- app/database/migrations/2014_09_22_203757_convert_block_classes_column_to_block_type_column.php
+++ app/database/migrations/2014_09_22_203757_convert_block_classes_column_to_block_type_column.php
@@ -5,32 +5,32 @@ use Illuminate\Database\Migrations\Migration;
 
 class ConvertBlockClassesColumnToBlockTypeColumn extends Migration {
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::table('blocks', function(Blueprint $table)
-		{
-			$table->dropColumn('block_classes');
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::table('blocks', function(Blueprint $table)
+    {
+      $table->dropColumn('block_classes');
       $table->string('block_type')->after('page_id');
-		});
-	}
+    });
+  }
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::table('blocks', function(Blueprint $table)
-		{
-			$table->dropColumn('block_type');
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::table('blocks', function(Blueprint $table)
+    {
+      $table->dropColumn('block_type');
       $table->text('block_classes')->after('page_id');
-		});
-	}
+    });
+  }
 
 }
