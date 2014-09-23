11df3dbc92ac9dc22133c81c658d2903fd1b58cc
Andrea Gaither <angaither@gmail.com>
2014-09-23 09:23:42 -0400

    Updated gpa min to go down to 3.0 Fixes #184

--- app/views/admin/scholarship/partials/_form_scholarship.blade.php
+++ app/views/admin/scholarship/partials/_form_scholarship.blade.php
@@ -72,7 +72,7 @@
 <div class="form-group">
   {{ Form::label('gpa_min', ' GPA Minimum : ') }}
   {{-- @TODO: figure out how to put a float here --}}
-  {{ Form::select('gpa_min', array('3.5', '3.6', '3.7', '3.8', '3.9'), null, ['class' => 'form-control']); }}
+  {{ Form::select('gpa_min', array('3.0', '3.1', '3.2', '3.3', '3.4', '3.5', '3.6', '3.7', '3.8', '3.9'), null, ['class' => 'form-control']); }}
   {{ errorsFor('gpa_min', $errors); }}
 </div>
 
