35dbf63e106578af906c83b6e109640604a435c7
Diego Lorenzo <dlorenzo@dosomething.org>
2014-09-24 18:23:57 -0400

    Initial work on cta block styles.

--- app/views/pages/partials/_block_cta.blade.php
+++ app/views/pages/partials/_block_cta.blade.php
@@ -1,7 +1,11 @@
 {{-- Block Type: Call To Action --}}
 
 <section class="segment segment--cta">
-  <h1 class="heading -alpha">{{ $block->block_title }}</h1>
+  <div class="wrapper">
 
-  {{ $block->block_body_html }}
+    <h1 class="heading -alpha">{{ $block->block_title }}</h1>
+
+    {{ $block->block_body_html }}
+
+  </div>
 </section>
