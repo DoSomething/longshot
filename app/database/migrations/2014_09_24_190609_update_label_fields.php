e10d722c87334d38f2a8247978b44a05ccc2720b
Andrea Gaither <angaither@gmail.com>
2014-09-24 16:59:19 -0400

    Change essay label fields from VARCHAR to text. Fixes #199

---
+++ app/database/migrations/2014_09_24_190609_update_label_fields.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateLabelFields extends Migration {
+
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    DB::statement("ALTER TABLE `scholarships` MODIFY COLUMN `label_app_essay1` TEXT");
+    DB::statement("ALTER TABLE `scholarships` MODIFY COLUMN `label_app_essay2` TEXT");
+  }
+
+}
