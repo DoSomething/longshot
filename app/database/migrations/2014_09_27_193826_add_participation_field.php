16811dd1d20732d717c87c0d4f80ddb5bcd83a87
Andrea Gaither <angaither@gmail.com>
2014-09-27 15:42:08 -0400

    Added participation field to app & scholarship tables.

---
+++ app/database/migrations/2014_09_27_193826_add_participation_field.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddParticipationField extends Migration {
+
+
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::table('applications', function($table)
+    {
+        $table->string('participation')->nullable()->after('activities');
+    });
+    Schema::table('scholarships', function($table)
+    {
+        $table->text('label_app_participation')->after('label_app_activities');
+    });
+  }
+
+  public function down()
+  {
+    Schema::table('applications', function($table)
+    {
+        $table->dropColumn('participation');
+    });
+    Schema::table('scholarships', function($table)
+    {
+        $table->dropColumn('label_app_participation');
+    });
+  }
+
+}
