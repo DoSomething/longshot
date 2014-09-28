f09344912ac39056c5ceb43b953e42f3e98964b9
Diego Lorenzo <diego@diegolorenzo.com>
2014-09-28 17:01:46 -0400

    Removing unnecessary attribute.

--- app/views/pages/partials/_block_cta.blade.php
+++ app/views/pages/partials/_block_cta.blade.php
@@ -7,12 +7,12 @@
     {{ $block->block_body_html }}
 
     @if (Auth::guest())
-      <div class="fragment" markdown="1">
+      <div class="fragment">
         {{ link_to_route('registration.create', 'Start Application', null, ['class' => 'button -default']) }}
         {{ link_to_route('status', 'or continue an application') }}
       </div>
     @else
-      <div class="fragment" markdown="1">
+      <div class="fragment">
         {{ link_to_route('status', 'Continue Application', null, ['class' => 'button -default']) }}
       </div>
     @endif
