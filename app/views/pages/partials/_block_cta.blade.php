fe8323d10e7f1dc848dc5d1ca21eaeacd845f77d
Diego Lorenzo <diego@diegolorenzo.com>
2014-09-28 17:03:30 -0400

    Update to make code cleaner.

--- app/views/pages/partials/_block_cta.blade.php
+++ app/views/pages/partials/_block_cta.blade.php
@@ -6,15 +6,13 @@
   <div class="wrapper">
     {{ $block->block_body_html }}
 
-    @if (Auth::guest())
-      <div class="fragment">
+    <div class="fragment">
+      @if (Auth::guest())
         {{ link_to_route('registration.create', 'Start Application', null, ['class' => 'button -default']) }}
         {{ link_to_route('status', 'or continue an application') }}
-      </div>
-    @else
-      <div class="fragment">
+      @else
         {{ link_to_route('status', 'Continue Application', null, ['class' => 'button -default']) }}
-      </div>
-    @endif
+      @endif
+    </div>
   </div>
 </section>
