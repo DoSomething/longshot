9f9a094e427d2a3932af72a259461a44d6dc9650
Diego Lorenzo <dlorenzo@dosomething.org>
2014-10-01 17:22:28 -0400

    Updating language per feedback.

--- app/views/layouts/partials/upgrade-message.blade.php
+++ app/views/layouts/partials/upgrade-message.blade.php
@@ -3,7 +3,7 @@
   <div class="wrapper">
     <h1>{{ $vars->company_name }}</h1>
     <p>Ruh-roh. Looks like your browser is out of date.</p>
-    <p>Upgrading it will give you a safer, more secure browsing experience and allow you to fully experience this and other modern websites.</p>
+    <p>Upgrading it will give you a safer, more secure browsing experience and allow you to fully explore this and other modern websites.</p>
     <a class="button -default" href="http://browsehappy.com/?locale=en" target="_blank" title="Please upgrade your browser!">Please upgrade your browser</a>
   </div>
 </div>
