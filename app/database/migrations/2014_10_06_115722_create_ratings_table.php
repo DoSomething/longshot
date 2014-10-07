72a2abc8133911a7ef9875e8782903c34c386d4f
Andrea Gaither <angaither@gmail.com>
2014-10-07 10:42:37 -0400

    Create rating table.

---
+++ app/database/migrations/2014_10_06_115722_create_ratings_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateRatingsTable extends Migration {
+
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('rating', function(Blueprint $table)
+    {
+      $table->increments('id');
+      $table->integer('application_id')->index();
+      $table->enum('rating', array('yes', 'no', 'maybe'));
+    });
+  }
+
+
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::drop('rating');
+  }
+
+}
