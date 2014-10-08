35fe4d2f0957d5b654bea9c1caf3bff440293c5b
Andrea Gaither <angaither@gmail.com>
2014-10-08 11:02:30 -0400

    This will fix the zeros saving on test score & gpa Fixes #355

---
+++ app/database/migrations/2014_10_08_105149_update_score_gpa_fields.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateScoreGpaFields extends Migration {
+
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    DB::statement('ALTER TABLE `applications` MODIFY `test_score` INTEGER unsigned NULL;');
+    DB::statement('ALTER TABLE `applications` MODIFY `gpa` FLOAT NULL;');
+
+  }
+
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    //
+  }
+
+}
