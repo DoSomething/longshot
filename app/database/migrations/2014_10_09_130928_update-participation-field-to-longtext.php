8db332d58a7614aa05127593eba5c5cdc09c00ff
Andrea Gaither <angaither@gmail.com>
2014-10-09 13:10:58 -0400

    Changed participation field from varcar to longtext. Fixes #419

---
+++ app/database/migrations/2014_10_09_130928_update-participation-field-to-longtext.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateParticipationFieldToLongtext extends Migration {
+
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    DB::statement("ALTER TABLE `applications` MODIFY COLUMN `participation` LONGTEXT");
+
+  }
+}
