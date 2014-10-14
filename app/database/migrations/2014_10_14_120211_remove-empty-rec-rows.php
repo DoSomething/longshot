cfa0320d04240022cf37be0c902c0f774a74a1b6
Andrea Gaither <angaither@gmail.com>
2014-10-14 12:12:48 -0400

    This removes all empty rec & rec tokens they were created during the great bug of columbus day weekend. Fixes #456

---
+++ app/database/migrations/2014_10_14_120211_remove-empty-rec-rows.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class RemoveEmptyRecRows extends Migration {
+
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    DB::statement("DELETE r.*, rt.*
+                   FROM recommendations r
+                   INNER JOIN recommendation_tokens rt on rt.recommendation_id = r.id
+                   WHERE r.first_name LIKE '';");
+
+  }
+}
+
