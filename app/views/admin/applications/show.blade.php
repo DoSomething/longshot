18058929d94b61b6879944d99057ff87592576d3
Andrea Gaither <angaither@gmail.com>
2014-10-20 10:46:56 -0400

    only show winner link  if marked as a yes.

--- app/views/admin/applications/show.blade.php
+++ app/views/admin/applications/show.blade.php
@@ -83,11 +83,12 @@
           {{ Form::close() }}
         @endif
 
-
+      @if ($app_rating && $app_rating == 'yes')
         {{ Form::open(['route' => 'admin.winner.store']) }}
         {{ Form::hidden('user_id', $id) }}
         {{ Form::submit('Make ' . $user['first_name'] . ' a winner', ['class' => 'btn btn-primary btn-lg', 'name' => 'winner']) }}
         {{ Form::close() }}
+      @endif
 
       </div>
 
