edfad458169a776e36331d3ea88d1ff93dbb096e
Andrea Gaither <angaither@gmail.com>
2014-10-28 15:52:13 -0400

    Get winners data from db, use a loop.

--- app/models/Winner.php
+++ app/models/Winner.php
@@ -14,4 +14,15 @@ class Winner extends Eloquent {
   {
     return $this->belongsTo('User');
   }
+
+  public static function getLastYearWinners()
+  {
+    $old_id = Scholarship::getCurrentScholarship()->pluck('id') - 1;
+    $winners = Winner::with('user')->where('scholarship_id', $old_id)->get();
+    foreach ($winners as $key => $winner) {
+      $winner->location = Profile::where('user_id', $winner->user->id)->select('city', 'state')->get();
+      $winner->gpa = Application::where('user_id', $winner->user->id)->pluck('gpa');
+    }
+    return $winners;
+  }
 }
