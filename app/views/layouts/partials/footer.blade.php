ec2f3a4742a2ee50e87d26ebe27f1a11a1568992
Diego Lorenzo <dlorenzo@dosomething.org>
2014-11-04 17:47:37 -0500

    Removing commented out code.

--- app/views/layouts/partials/footer.blade.php
+++ app/views/layouts/partials/footer.blade.php
@@ -5,16 +5,6 @@
       <span>In partnership with:</span> <img src="{{ $global_vars->footer_logo or '/dist/images/tmi-logo.png' }}" alt="partner">
     </div>
 
-    {{--
-    <nav class="alternative-nav">
-      <ul class="__menu -level-1">
-        <li><a href="/about"><span>About</span></a></li>
-        <li><a href="/faq"><span>FAQs</span></a></li>
-        <li><a href="{{ URL::to('https://s3.amazonaws.com/uploads.hipchat.com/34218/1222616/xWTSRkWcE7jrdAD/Foot%20Locker%20Scholar%20Athletes%20Official%20Rules%202014-2015.pdf') }}" target="_blank"><span>Official Rules</span></a></li>
-      </ul>
-    </nav>
-    --}}
-
     <p class="__copyright">Copyright &copy; {{ date('Y') }} {{ $global_vars->company_name or 'TMI Agency' }}</p>
 
     <small class="__message">
