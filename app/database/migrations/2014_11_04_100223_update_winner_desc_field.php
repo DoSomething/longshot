d30e36fb2f00335f19eba451cc86736ea8f3c819
Andrea Gaither <angaither@gmail.com>
2014-11-04 10:35:45 -0500

    Update winner desc from varcar to text Fixes #515

---
+++ app/database/migrations/2014_11_04_100223_update_winner_desc_field.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateWinnerDescField extends Migration {
+
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    DB::statement("ALTER TABLE `winners` MODIFY COLUMN `description` TEXT");
+
+  }
+
+}
