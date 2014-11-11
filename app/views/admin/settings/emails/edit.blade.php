daf7013cf7cf62781e9f74b82d92d5d1323fa515
Diego Lorenzo <dlorenzo@dosomething.org>
2014-11-11 17:37:05 -0500

    Updating to use bower for bootstrap and some fixes.

--- app/views/admin/settings/emails/edit.blade.php
+++ app/views/admin/settings/emails/edit.blade.php
@@ -5,50 +5,45 @@
     <div class="row">
 
       @include('admin.layouts.partials.subnav-settings')
-      {{ Form::open(['route' => 'emails.update']) }}
-
 
       <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
-        <h1 class="page-header">Emails!</h1>
 
+        {{ Form::open(['route' => 'emails.update']) }}
 
-      <div class="panel-group" id="accordion">
-          <div class="panel panel-default">
-            <div class="panel-heading">
-              <h4 class="panel-title">
-                  Applicant Emails
-              </h4>
-            </div>
-              <div class="panel-body">
-                  @foreach($applicant_emails as $key => $email)
+          <h1 class="page-header">Emails!</h1>
 
-                  @include('admin.settings.emails.partials._form_emails',  array('class' => 'applicant'))
-
-                  @endforeach
-
+          <div class="panel-group" id="accordion">
+            <div class="panel panel-default">
+              <div class="panel-heading">
+                <h4 class="panel-title">Applicant Emails</h4>
               </div>
+              <div class="panel-body">
+                @foreach($applicant_emails as $key => $email)
+                  @include('admin.settings.emails.partials._form_emails',  array('class' => 'applicant'))
+                @endforeach
+              </div>
+            </div>
           </div>
 
-    <div class="panel-group" id="accordion">
-          <div class="panel panel-default">
-            <div class="panel-heading">
-              <h4 class="panel-title">
-                  Recommender Emails
-              </h4>
-            </div>
-              <div class="panel-body">
-
-                  @foreach($recommender_emails as $key => $email)
-                     @include('admin.settings.emails.partials._form_emails', array('class' => 'recommender'))
-                  @endforeach
-
+          <div class="panel-group" id="accordion">
+            <div class="panel panel-default">
+              <div class="panel-heading">
+                <h4 class="panel-title">Recommender Emails</h4>
               </div>
+              <div class="panel-body">
+                @foreach($recommender_emails as $key => $email)
+                   @include('admin.settings.emails.partials._form_emails', array('class' => 'recommender'))
+                @endforeach
+              </div>
+            </div>
+          </div>
+
+          <div>
+            {{ Form::submit('Update Email Settings', ['class' => 'btn btn-default']) }}
           </div>
 
         {{ Form::close() }}
-         <div>
-            {{ Form::submit('Update Email Settings', ['class' => 'btn btn-default']) }}
-          </div>
+
       </div>
 
     </div>
