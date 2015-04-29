d65aeb3ef47c9c9dfbcc1a5c3a39edc782633668
Andrea Gaither <angaither@gmail.com>
2015-04-29 10:30:42 -0400

    If the applicant role exists, remove it & all users with that role Fixes #656

---
+++ app/database/migrations/2015_04_29_101023_remove_app_roles.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class RemoveAppRoles extends Migration {
+
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    // if the applicant role is the the db, remove it and all corresponding role_user records
+    $role = DB::table('roles')->where('name', 'applicant')->first();
+    if (!is_null($role)) {
+      DB::table('role_user')->where('role_id', 2)->delete();
+      DB::table('roles')->where('name', 'applicant')->delete();
+    }
+  }
+}
