914e62ee280da9be39b7b6ab10b10e5e3bd60d1e
Shae Smith <ssmith@dosomething.org>
2015-08-26 13:08:50 -0400

    configure gulp-sass and import susy correctly

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -10,14 +10,7 @@ var config       = require('../util/config');
 
 gulp.task('sass', function () {
   return gulp.src(config.sassSrc + '/**/*.scss')
-    .pipe(sass({
-      trace: true,
-      bundleExec: true,
-      require: 'susy',
-      style: 'compressed',
-      sourcemapPath: config.sassSrc,
-      cacheLocation: config.sassSrc + '/.sass-cache',
-    }))
+    .pipe(sass({outputStyle: 'compressed'}))
     .on('error', handleErrors)
     .pipe(autoprefixer('last 5 version'))
     .pipe(gulp.dest(config.sassDest));
