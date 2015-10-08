a4bfacc0ed57af3e7c17e8fc3b790f2580f10823
Katie Crane <katiethecrane@gmail.com>
2015-10-08 13:55:53 -0400

    Update _form-password-field.blade.php
    fixed to use literal syntax

--- app/views/layouts/partials/_form-password-field.blade.php
+++ app/views/layouts/partials/_form-password-field.blade.php
@@ -1,5 +1,5 @@
 <div class="field-group {{ setInvalidClass('password', $errors) }}">
   {{ Form::label('password', 'Password: ') }}
-  {{ Form::password('password', array('placeholder' => 'Password must be at least six characters long')) }}
+  {{ Form::password('password', ['placeholder' => 'Password must be at least six characters long']) }}
   {{ errorsFor('password', $errors); }}
 </div>
