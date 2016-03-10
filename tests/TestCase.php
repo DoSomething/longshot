d64b2b90e67999a8b5484870744e3867f511549f
katiecrane <katiethecrane@gmail.com>
2016-03-10 11:18:02 -0500

    start.php isn't a thing anymore

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -15,6 +15,6 @@ class TestCase extends Illuminate\Foundation\Testing\TestCase
 
       $testEnvironment = 'testing';
 
-      return require __DIR__.'/../../bootstrap/start.php';
+      return require __DIR__.'/../../bootstrap/app.php';
   }
 }
