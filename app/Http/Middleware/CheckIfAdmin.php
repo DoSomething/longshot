456168fb70038f9cf675ac0a2d25c5f6175702f1
katiecrane <katiethecrane@gmail.com>
2016-04-27 14:10:02 -0400

    add admin middleware

---
+++ app/Http/Middleware/CheckIfAdmin.php
@@ -0,0 +1,33 @@
+<?php namespace App\Http\Middleware;
+
+use Closure;
+use Scholarship\Repositories\SettingRepository;
+use Auth;
+
+class CheckIfAdmin
+{
+    /**
+     * @var SettingsRepository
+     */
+    protected $settings;
+
+    public function __construct(SettingRepository $settings)
+    {
+        $this->settings = $settings;
+    }
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        // Is current user an admin?
+        if (!Auth::user() || !Auth::user()->hasRole('administrator')) {
+            return redirect()->home()->with('flash_message', ['text' => 'You are not authorized to view that page.', 'class' => '-error']);
+        }
+        return $next($request);
+    }
+}
