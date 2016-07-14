247d48498270a69471a80be0b3f8b90cda9fd2af
katiecrane <kcrane@dosomething.org>
2016-07-14 12:56:27 -0400

    SSSTTTYYYLLLEEE

--- database/seeds/AppSubmitHelpTextTableSeeder.php
+++ database/seeds/AppSubmitHelpTextTableSeeder.php
@@ -8,9 +8,9 @@ class AppSubmitHelpTextTableSeeder extends Seeder
     public function run()
     {
         Setting::insert([
-       'category'    => 'general',
-         'key'       => 'application_submit_help_text',
-         'value'     => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque facilis, explicabo quidem ea dolore fugit.',
+        'category'     => 'general',
+          'key'        => 'application_submit_help_text',
+          'value'      => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque facilis, explicabo quidem ea dolore fugit.',
           'type'      => 'textarea',
        ]);
     }
