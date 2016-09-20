c12095dcaa5b14fb13137e80602e7bdc8c1c8e65
blisteringherb <morgan@solosuenos.com>
2016-09-20 10:58:55 -0400

    Adding symlink for content/images

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,6 +23,8 @@ namespace :deploy do
 
   task :link_folders do
     run "ln -nfs #{shared_path}/.env #{release_path}/"
+    run "ln -nfs #{shared_path}/content/images #{release_path}/public"
+
     folders.each do |folder|
       run "rm -rf #{release_path}/storage/#{folder}"
       run "ln -nfs #{shared_path}/#{folder} #{release_path}/storage/#{folder}"
