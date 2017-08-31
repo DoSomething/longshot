7882ca7fd54dd97c3fa99112187063f1cc6d0289
katiecrane <kcrane@dosomething.org>
2017-08-31 10:47:33 -0400

    Migration for new scholarships column

---
+++ database/migrations/2017_08_31_144335_add_image_uploads_column_to_scholarships_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddImageUploadsColumnToScholarshipsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('scholarships', function ($table) {
+            $table->boolean('image_uploads')->after('label_app_essay2')->nullable()->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('scholarships', function ($table) {
+            $table->dropColumn('image_uploads');
+        });
+    }
+}
