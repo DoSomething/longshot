e51e389fc05e0a8a02eab999c6fbf10b21aa8e16
katiecrane <kcrane@dosomething.org>
2017-11-09 11:10:18 -0500

    Copy updates

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -6,7 +6,7 @@
   <div class="container">
     <h1>Welcome, {{ $user->first_name }}!</h1>
 
-    <p>There are a total of <strong>{{ $count['users'] }}</strong> users in the system.</p>
+    <p>There are a total of <strong>{{ $count['users'] }}</strong> applicants in the system.</p>
     <p>{!! link_to_route('applications.index', 'View all Applications', null, ['class' => 'btn btn-primary btn-lg']) !!}</p>
   </div>
 </div>
@@ -17,7 +17,7 @@
 
   <div class="container">
     <div class="row">
-
+      <p>Below are counts of certain subsets of users and nominators. Some of these subsets overlap. The total number of applicants can be found above.</p>
        <table class="table table-striped">
         <thead>
           <tr>
@@ -43,23 +43,23 @@
             <td> {{ $count['unique_recs'] }} </td>
           </tr>
           <tr>
-            <td> Submitted Applications </td>
+            <td> Submitted Applications (including those that are submitted AND completed)</td>
             <td> {{ $count['submitted_apps'] }} </td>
           </tr>
           <tr>
-            <td> Completed Applications <em> (these are DONE) </em> </td>
+            <td> Completed Applications <em> (These are DONE. Submitted AND completed.) </em> </td>
             <td> {{ $count['completed_apps'] }} </td>
           </tr>
           <tr>
-            <td> Applications with one requested recommendation </td>
+            <td> Applications with one requested recommendation <em> (could have requested a second recommendation that is complete) </em></td>
             <td> {{ $count['requested_one'] }} </td>
           </tr>
            <tr>
-            <td> Applications with one completed recommendation </td>
+            <td> Applications with one completed recommendation <em>(could have requested a second recommendation that is incomplete)</em></td>
             <td> {{ $count['submitted_one'] }} </td>
           </tr>
           <tr>
-            <td> Applications with two requested recommendations </td>
+            <td> Applications with two requested recommendations <em>(both unfinished)</em></td>
             <td> {{ $count['requested_two'] }} </td>
           </tr>
           <tr>
