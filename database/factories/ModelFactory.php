4abdd913ac0a94833102c823578b1e8382bc5079
katiecrane <kcrane@dosomething.org>
2018-01-11 14:33:27 -0500

    Finish creating all of the factories

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -1,6 +1,11 @@
 <?php
+
 use Faker\Generator;
 use App\Models\Profile;
+use App\Models\Race;
+use App\Models\Application;
+use App\Models\Recommendation;
+use App\Models\User;
 
 /**
 |--------------------------------------------------------------------------
@@ -22,115 +27,139 @@ use App\Models\Profile;
 // user with finished profile and submitted app and rec requests
 // user with finished profile, completed app, completed rec requests
 
-
-
 // User Factory
-        // need:
-        // first_name
-        // last_name
 $factory->define(User::class, function (Faker\Generator $faker) {
     return [
-        'name' => $faker->name,
+        'first_name' => $faker->firstName,
+        'last_name' => $faker->lastName,
         'email' => $faker->safeEmail,
         'password' => bcrypt(str_random(10)),
         'remember_token' => str_random(10),
     ];
 });
 
-// Profile Factory
-    // birthdate
-    // phone
-    // address_street
-    // address_premise
-    // city
-    // state
-    // zip
-    // gender
-    // school
-    // grade
+// Profile Factories
+    // user_id
 $factory->define(Profile::class, function (Generator $faker) {
     return [
         'birthdate' => $faker->dateTimeInInterval($startDate = '-18 years', $interval = '+1 years')->format('Y-m-d'),
-        'phone' => $faker->phoneNumber,
-        'address_street' => null,
-        'address_premise' => null,
-        'city' => null,
-        'state' => null,
-        'zip' => null,
-        'gender' => null,
-        'school' => null,
-        'grade' => null,
+        'phone' => $faker->randomNumber(9) . $faker->randomNumber(1),
+        'address_street' => $faker->streetAddress(),
+        'address_premise' => $faker->randomElement(['', '', $faker->secondaryAddress]),
+        'city' => $faker->city,
+        'state' => $faker->state,
+        'zip' => $faker->randomNumber(5),
+        'gender' => $faker->randomElement(['M', 'F']),
+        'school' => $faker->firstNameFemale . ' ' . $faker->lastName . ' High School',
+        'grade' => $faker->numberBetween(9, 12),
     ];
 });
 
 
 // Partial Profile factory
-    // birthdate
-    // phone
+    // user_id
+$factory->defineAs(Profile::class, 'partial', function (Generator $faker) {
+    return [
+        'birthdate' => $faker->dateTimeInInterval($startDate = '-18 years', $interval = '+1 years')->format('Y-m-d'),
+        'phone' => $faker->randomNumber(9) . $faker->randomNumber(1),
+    ];
+});
 
 // Races (goes with profile and partial profile)
     // profile_id
-    // race
+$factory->define(Race::class, function (Generator $faker) {
+    return [
+        'race' => $faker->randomElement(['White', 'Black/African American', 'Hispanic/Latino/Spanish', 'Asian', 'American Indian or Alaska Native', 'Pacific Islander/Native Hawaiian', 'Other']),
+    ];
+});
 
-// Applications (need some partial)
+// Application ("finished" but not submitted)
     // scholarship_id
-    // accomplishments
-    // activities
-    // participation
-    // essay1
-    // essay2
-    // hear_about
-    // test_type
-    // test_score
-    // gpa
+    // user_id
+$factory->define(Application::class, function (Generator $faker) {
+    return [
+        'accomplishments' => $faker->paragraphs(4, true),
+        'activities' => $faker->paragraph(5),
+        'participation' => $faker->paragraph(5),
+        'essay1' => $faker->paragraphs(6, true),
+        'essay2' => $faker->paragraphs(6, true),
+        'hear_about' => $faker->randomElement(['Online', 'Search Engine', 'Friend', '']),
+        'test_type' => $faker->randomElement(['PSAT', 'ACT', 'SAT (1600)', 'SAT (2400)']),
+        'test_score' => $faker->numberBetween(0, 2400),
+        'gpa' => $faker->randomElement([3.0, 3.1, 3.2, 3.3, 3.4, 3.5, 3.6, 3.7, 3.8, 3.9, 4.0]),
+    ];
+});
 
-// submitted app
+// Submitted Application
     // scholarship_id
-    // accomplishments
-    // activities
-    // participation
-    // essay1
-    // essay2
-    // hear_about
-    // test_type
-    // test_score
-    // gpa
-    // submitted
+    // user_id
+$factory->defineAs(Application::class, 'submitted', function (Generator $faker) {
+    return [
+        'accomplishments' => $faker->paragraphs(4, true),
+        'activities' => $faker->paragraph(5),
+        'participation' => $faker->paragraph(5),
+        'essay1' => $faker->paragraphs(6, true),
+        'essay2' => $faker->paragraphs(6, true),
+        'hear_about' => $faker->randomElement(['Online', 'Search Engine', 'Friend', '']),
+        'test_type' => $faker->randomElement(['PSAT', 'ACT', 'SAT (1600)', 'SAT (2400)']),
+        'test_score' => $faker->numberBetween(0, 2400),
+        'gpa' => $faker->randomElement([3.0, 3.1, 3.2, 3.3, 3.4, 3.5, 3.6, 3.7, 3.8, 3.9, 4.0]),
+        'submitted' => 1,
+    ];
+});
 
-// completed app
+// Completed Application
     // scholarship_id
-    // accomplishments
-    // activities
-    // participation
-    // essay1
-    // essay2
-    // hear_about
-    // test_type
-    // test_score
-    // gpa
-    // submitted
-    // completed
+    // user_id
+$factory->defineAs(Application::class, 'completed', function (Generator $faker) {
+    return [
+        'accomplishments' => $faker->paragraphs(4, true),
+        'activities' => $faker->paragraph(5),
+        'participation' => $faker->paragraph(5),
+        'essay1' => $faker->paragraphs(6, true),
+        'essay2' => $faker->paragraphs(6, true),
+        'hear_about' => $faker->randomElement(['Online', 'Search Engine', 'Friend', '']),
+        'test_type' => $faker->randomElement(['PSAT', 'ACT', 'SAT (1600)', 'SAT (2400)']),
+        'test_score' => $faker->numberBetween(0, 2400),
+        'gpa' => $faker->randomElement([3.0, 3.1, 3.2, 3.3, 3.4, 3.5, 3.6, 3.7, 3.8, 3.9, 4.0]),
+        'submitted' => 1,
+        'completed' => 1,
+    ];
+});
 
-// partial app
+// Partial Application
     // scholarship_id
-    // accomplishments
-    // activities
+    // user_id
+$factory->defineAs(Application::class, 'partial', function (Generator $faker) {
+    return [
+        'accomplishments' => $faker->paragraphs(4, true),
+        'activities' => $faker->paragraph(5),
+    ];
+});
 
 // Recommendation Requests
     // application_id
-    // first_name
-    // last_name
-    // relationship
-    // phone
-    // email
+$factory->defineAs(Recommendation::class, 'request', function (Generator $faker) {
+    return [
+        'first_name' => $faker->firstName(),
+        'last_name' => $faker->lastName,
+        'relationship' => $faker->randomElement(['Teacher', 'Coach', 'Mentor', 'Band Director', 'Club Sponsor', 'Parent']),
+        'phone' => $faker->randomNumber(9) . $faker->randomNumber(1),
+        'email' => $faker->safeEmail,
+    ];
+});
 
 // Completed Recommendations
     // application_id
-    // first_name
-    // last_name
-    // relationship
-    // phone
-    // email
-    // rank_character
-    // rank_additional
-    // essay1
+$factory->defineAs(Recommendation::class, 'complete', function (Generator $faker) {
+    return [
+        'first_name' => $faker->firstName(),
+        'last_name' => $faker->lastName,
+        'relationship' => $faker->randomElement(['Teacher', 'Coach', 'Mentor', 'Band Director', 'Club Sponsor', 'Parent']),
+        'phone' => $faker->randomNumber(9) . $faker->randomNumber(1),
+        'email' => $faker->safeEmail,
+        'rank_character' => $faker->randomElement(Recommendation::getRankValues()),
+        'rank_additional' => $faker->randomElement(Recommendation::getRankValues()),
+        'essay1' => $faker->paragraph(5),
+    ];
+});
