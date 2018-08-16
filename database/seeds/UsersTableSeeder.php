fed7ba3512e3d72bf42a6425115df63d01fedb08
katiecrane <kcrane@dosomething.org>
2018-08-16 12:10:13 -0700

    Just use admin email

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -18,40 +18,5 @@ class UsersTableSeeder extends Seeder
         'email'      => 'admin@dosomething.org',
         'password'   => 'secret',
       ])->assignRole(1);
-
-        User::create([
-        'first_name' => 'Katie',
-        'last_name'  => 'Crane',
-        'email'      => 'kcrane@dosomething.org',
-        'password'   => 'secret',
-      ])->assignRole(1);
-
-        User::create([
-        'first_name' => 'Jen',
-        'last_name'  => 'Ng',
-        'email'      => 'jng@dosomething.org',
-        'password'   => 'secret',
-      ])->assignRole(1);
-
-        User::create([
-        'first_name' => 'Mai',
-        'last_name'  => 'Irie',
-        'email'      => 'mirie@dosomething.org',
-        'password'   => 'secret',
-      ])->assignRole(1);
-
-        User::create([
-        'first_name' => 'Dave',
-        'last_name'  => 'Furnes',
-        'email'      => 'dfurnes@dosomething.org',
-        'password'   => 'secret',
-      ])->assignRole(1);
-
-        User::create([
-        'first_name' => 'Chloe',
-        'last_name'  => 'Lee',
-        'email'      => 'clee@dosomething.org',
-        'password'   => 'secret',
-      ])->assignRole(1);
     }
 }
