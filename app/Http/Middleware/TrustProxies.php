9e180ca83566d53b7f3676e9a7e460aa76877417
David Furnes <david@dfurnes.com>
2018-11-16 15:35:36 -0500

    Add TrustProxies middleware.

---
+++ app/Http/Middleware/TrustProxies.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Aurora\Http\Middleware;
+
+use Illuminate\Http\Request;
+use Fideloper\Proxy\TrustProxies as Middleware;
+
+class TrustProxies extends Middleware
+{
+    /**
+     * The trusted proxies for this application,
+     * sourced from 'config/trustedproxy.php'.
+     *
+     * @var array
+     */
+    protected $proxies;
+
+    /**
+     * The current proxy header mappings.
+     *
+     * @var array
+     */
+    protected $headers = [
+        Request::HEADER_FORWARDED => null, // Not set on AWS or Heroku.
+        Request::HEADER_X_FORWARDED_FOR => 'X_FORWARDED_FOR',
+        Request::HEADER_X_FORWARDED_HOST => null, // Not set on AWS or Heroku.
+        Request::HEADER_X_FORWARDED_PORT => 'X_FORWARDED_PORT',
+        Request::HEADER_X_FORWARDED_PROTO => 'X_FORWARDED_PROTO',
+    ];
+}
