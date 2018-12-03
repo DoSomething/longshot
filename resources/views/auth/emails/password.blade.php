adb9f20a87af730d888eb8c13da19355e3cb750a
Chloe Lee <chloealessandra@gmail.com>
2018-12-03 10:49:27 -0500

    adds more breaks

--- resources/views/auth/emails/password.blade.php
+++ resources/views/auth/emails/password.blade.php
@@ -1,7 +1,7 @@
-Hi!
+Hi!<br/>
 <br/>
 You are receiving this email because we received a password reset request from you. If you requested this, please click here to reset your password: <a href="{{ $link = url('password/reset', $token).'?email='.urlencode($user->getEmailForPasswordReset()) }}"> {{ $link }} </a><br/>
 <br/>
-If you did not request a password reset, you can ignore this email. Your password will not change and your account is safe.
+If you did not request a password reset, you can ignore this email. Your password will not change and your account is safe.<br/>
 <br/>
 Thanks!
